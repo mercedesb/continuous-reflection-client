0be1c6fadf79e473eca37ea3415dcdce430d170f
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-03-01 16:06:45 -0600

    decrease spacing on Wrapper

--- src/_shared/Wrapper.js
+++ src/_shared/Wrapper.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 export function Wrapper({ children, className }) {
-  const classes = `flex flex-col w-3/4 mx-auto my-12 ${className}`
+  const classes = `flex flex-col w-5/6 mx-auto my-12 ${className}`
   return <div className={classes}>{children}</div>
 }
