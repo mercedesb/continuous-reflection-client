6d440625074b7beb5555207b706580d05271ceef
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-02-29 18:37:55 -0600

    style and add more info to list items

--- src/domains/JournalList/JournalListItem.js
+++ src/domains/JournalList/JournalListItem.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import { DATE_FORMAT } from 'utils'
 
 export function JournalListItem({ journal }) {
+  const hasEntries = journal.journalEntries && journal.journalEntries.length > 0
+
   return (
-    <div className='my-4'>
-      <Link to={`/journals/${journal.id}`}>{journal.name}</Link>
-    </div>
+    <Link to={`/journals/${journal.id}`} className='no-underline'>
+      <div className='my-4 border rounded px-8 py-4 border-gray-400 flex'>
+        <div className='flex-1'>
+          <h2>{journal.name}</h2>
+          <p className='my-4 font-semibold'>{journal.template} Journal</p>
+          {hasEntries && (
+            <p className='italic text-xs'>
+              Lastest entry: {moment(journal.journalEntries[0].entryDate).format(DATE_FORMAT)}
+            </p>
+          )}
+        </div>
+        <div className='flex-1'>
+          <p className='my-4 text-right'>
+            {hasEntries ? journal.journalEntries.length : 0} Entries
+          </p>
+        </div>
+      </div>
+    </Link>
   )
 }
